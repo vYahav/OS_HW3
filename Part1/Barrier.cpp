f0db268d6f782cced90c26ff86d8b7b2427d1d62
vYahav <51291115+vYahav@users.noreply.github.com>
2020-05-30 16:43:17 +0300

    Add files via upload

--- Part1/Barrier.cpp
+++ Part1/Barrier.cpp
@@ -1,12 +1,20 @@
 #include "Barrier.h"
 
 Barrier::Barrier(unsigned int num_of_threads){
-	this->num_of_threads=num_of_threads;
-	sem_init(&sem,0,num_of_threads);
+	this->N=num_of_threads;
+	sem_init(&sem,0,this->N);
 }
 
 void Barrier::wait(){
-	sem_wait(&sem);
+	sem_trywait(&sem);
+	int threadCounter;
+	while(sem_getvalue(&sem,&threadCounter)>0);//Wait because there is no N threads
+												//waiting.
+												
+	//When there are N threads waiting, restart sem for the next time
+	//we'll use the barrier.
+	sem_destroy(&sem); 
+	sem_init(&sem,0,this->N);
 }
 
 Barrier::~Barrier(){
--- Part1/Barrier.h
+++ Part1/Barrier.h
@@ -4,14 +4,14 @@
 #include <semaphore.h>
 
 class Barrier {
+private:
+	unsigned int N;
+	sem_t sem;
 public:
     Barrier(unsigned int num_of_threads);
     void wait();
     ~Barrier();
-    // TODO: define the member variables
-	// Remember: you can only use semaphores!
-	unsigned int num_of_threads;
-	sem_t sem;
+    
 };
 
 #endif // BARRIER_H_
